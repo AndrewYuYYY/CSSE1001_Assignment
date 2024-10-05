fc62435ad3538d25e88233c8e4d024b7882beb8d
Yiyang Yu <andrewyuyy@gmail.com>
2024-10-05 17:29:56 +1000

    Add some methods
    get_targets done
    have a default constructor for weapon class

--- Projects/Assignment 2/a2/a2.py
+++ Projects/Assignment 2/a2/a2.py
@@ -10,17 +10,17 @@ def main():
     # Implement your main function here
     pass
 
-class Weapon():
+class Weapon:
     """
     A class to display details of a weapon in the game.
     """
 
     def __init__(
             self,
-            symbol: str,
-            name: str,
-            effect: dict[str, int],
-            range: int
+            symbol = 'W',
+            name = 'AbstractWeapon',
+            effect = {},
+            range = 0
     ) -> None:
         """
         Constructor for Weapon class.
@@ -37,6 +37,7 @@ class Weapon():
         self._effect = effect
         self._range = range
 
+
     def get_name(self) -> str:
         """
         Returns the name of the weapon.
@@ -44,7 +45,7 @@ class Weapon():
         Return:
             A string of the name of the weapon.
         """
-        return f"{self._name}"
+        return f'{self._name}'
 
     def get_symbol(self) -> str:
         """
@@ -53,7 +54,8 @@ class Weapon():
         Return:
             A string of the symbol of the weapon.
         """
-        return f"{self._symbol}"
+
+        return f'{self._symbol}'
 
     def get_effect(self) -> dict[str, int]:
         """
@@ -74,7 +76,22 @@ class Weapon():
         Return:
             A list of positions in range.
         """
-        pass
+
+        # Convert the Position into x, y coordinates of the weapon.
+        x, y = position
+        # Create an empty list to store the targets.
+        targets = []
+
+        #Use two for-loops to check all the positions in the weapon's range.
+        for dx in range(-self._range, self._range + 1):
+            for dy in range(-self._range, self._range + 1):
+                #Use an if-statement to check if the
+                #manhattan distance is in the weapon's range,
+                #if so, append the position to the targets list.
+                if abs(dx) + abs(dy) <= self._range:
+                    targets.append((x + dx, y + dy))
+
+        return targets
 
     def __str__(self) -> str:
         """
@@ -83,8 +100,34 @@ class Weapon():
         Return:
             A string of the name of the weapon.
         """
-        return f"{self._name}"
+        return f'{self._name}'
 
+    def __repr__(self) -> str:
+        """
+        Returns the instance which can be used to
+        create a new identical weapon instance.
+
+        Return:
+            A string of the instance.
+        """
+        return (f'Weapon({self._symbol}, '
+                f'{self._name}, '
+                f'{self._effect}, '
+                f'{self._range})'
+                )
+
+class PoisonDart(Weapon):
+    """
+    A class to display details of a Poison Dart weapon in the game.
+    """
+
+    def __init__(self) -> None:
+        """
+        Constructor for PoisonDart class.
+        """
+
+        # Use super() to call the __init__ method from the parent class.
+        super().__init__('D', 'Poison Dart', {'poison': 2}, 2)
 
 
 
